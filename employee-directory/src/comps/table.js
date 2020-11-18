7575628a4e382e1cabc2ad2c5395197db6c322f4
JonM <jonmisner86@gmail.com>
2020-11-18 15:03:26 -0600

    added table

--- employee-directory/src/comps/table.js
+++ employee-directory/src/comps/table.js
@@ -1,1 +1,34 @@
-import 
+import  React from "react";
+import DataBody from "./dataBody";
+
+function Table ({ headings, users, handleSort}) {
+   return (
+      <div className="datatable mt-5">
+         <table
+            id="table"
+            className="tabnle table-Striped table-hover table-condensed">
+            <thread>
+               <tr>
+                  {headings.map(({name, width}) => {
+                     return (
+                        <th
+                           className="col"
+                           key={name}
+                           style={{width}}
+                           onCLick= {() => {
+                              handleSort(name.toLowerCase());
+                           }}>
+                           {name}
+                           <span className="pointer"></span> 
+                        </th>
+                     );
+                  })}
+               </tr>
+            </thread>
+            <DataBody users={users}/>
+         </table>
+      </div>
+   );
+}
+
+export default Table;
