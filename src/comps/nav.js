581be116d4a6c4d7b7f18bb3ebd07ba8ddd87326
JonM <jonmisner86@gmail.com>
2020-12-08 22:29:15 -0600

    tweaks

--- src/comps/nav.js
+++ src/comps/nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Collapse,
   Navbar,
@@ -6,32 +6,30 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-} from 'reactstrap';
+} from "reactstrap";
 import Search from "./search";
 // import Body from "./body";
-import "../styles/nav.css"
+import "../styles/nav.css";
 
-const NavBar = ({handleSearchChange}) => {
+const NavBar = ({ handleSearchChange }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div >
-      <Navbar className="nav" expand="md" >
+    <div>
+      <Navbar className="nav" expand="md">
         <NavbarBrand href="/">Employee Directory</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-          </Nav>
+          <Nav className="mr-auto" navbar></Nav>
           <NavItem>
-            <Search handleSearchChange = {handleSearchChange} />
+            <Search handleSearchChange={handleSearchChange} />
           </NavItem>
         </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
 export default NavBar;
-
--- src/comps/search.js
+++ src/comps/search.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
-
+import React from "react";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 
 // search form/button
-function Search ({handleSearchChange}) {
-   return (
-      <Form>
-         <FormGroup>
-         <Label for="exampleSearch"></Label>
-         <Input
-            type="search"
-            name="search"
-            id="exampleSearch"
-            placeholder="Search Here"
-            onChange={e => handleSearchChange(e)}
-         />
-         </FormGroup>
-      </Form>
-   );
+function Search({ handleSearchChange }) {
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="exampleSearch"></Label>
+        <Input
+          type="search"
+          name="search"
+          id="exampleSearch"
+          placeholder="Search Here"
+          onChange={(e) => handleSearchChange(e)}
+        />
+      </FormGroup>
+    </Form>
+  );
 }
-
 
 export default Search;
