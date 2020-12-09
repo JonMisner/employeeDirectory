a2680dde8bd8d798e54fd210356fd471405f3cb5
JonM <jonmisner86@gmail.com>
2020-12-08 21:32:43 -0600

    build script

--- employee-directory/src/comps/table.js
+++ employee-directory/src/comps/table.js
@@ -1,32 +1,34 @@
-import  React from "react";
+import React from "react";
 import DataBody from "./dataBody";
 
-export default function Table ({ headings, users, handleSort}) {
-   return (
-      <div className="datatable mt-5">
-         <table
-            id="table"
-            className="tabnle table-Striped table-hover table-condensed">
-            <thread>
-               <tr>
-                  {headings.map(({name, width}) => {
-                     return (
-                        <th
-                           className="col"
-                           key={name}
-                           style={{width}}
-                           onCLick= {() => {
-                              handleSort(name.toLowerCase());
-                           }}>
-                           {name}
-                           <span className="pointer"></span> 
-                        </th>
-                     );
-                  })}
-               </tr>
-            </thread>
-            <DataBody users={users}/>
-         </table>
-      </div>
-   );
+export default function Table({ headings, users, handleSort }) {
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="tabnle table-Striped table-hover table-condensed"
+      >
+        <thread>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onCLick={() => {
+                    handleSort(name.toLowerCase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thread>
+        <DataBody users={users} />
+      </table>
+    </div>
+  );
 }
--- employee-directory/src/pages/page.js
+++ employee-directory/src/pages/page.js
@@ -2,35 +2,20 @@ import React from "react";
 // import API from "../utils/API";
 // import NavBar from "../comps/nav";
 import Body from "../comps/body";
-import { Jumbotron, Container } from 'reactstrap';
+import { Jumbotron, Container } from "reactstrap";
 
-
-function Page () {
-   
-   
-   return (
-      // <Router>
-        <div>
-         <Jumbotron fluid>
-            <Container fluid>
-               <Body/>
-            </Container>
-         </Jumbotron>
-        </div>
-      // </Router>
-    );
-
+function Page() {
+  return (
+    // <Router>
+    <div>
+      <Jumbotron fluid>
+        <Container fluid>
+          <Body />
+        </Container>
+      </Jumbotron>
+    </div>
+    // </Router>
+  );
 }
 
-
-// bring in and format API info (set to state)
-// 
-// all state manipulating functions on here
-
-
-   // render function
-      // nav bar /search
-      // header
-      // body
-         // employees
- export default Page;
+export default Page;
